65124d9d8921630462bdca1769f6f6ff5df99762
Bill Oxbury <oxburybill@gmail.com>
2023-02-25 09:49:52 +0000

    Add About

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,115 @@
+var store = [{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/post-modified/",
+        "teaser": "/assets/img/2019-07-01/zip2.png"
+      },{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/post-standard/",
+        "teaser": "/assets/img/2019-07-01/zip2.png"
+      },{
+        "title": "Post: Chat",
+        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["Blog"],
+        "tags": ["chat","Post Formats"],
+        "url": "/blog/post-chat/",
+        "teaser": "/assets/img/2019-07-01/zip2.png"
+      },{
+        "title": "Post: Notice",
+        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
+        "tags": ["Post Formats","notice"],
+        "url": "/blog/post-notice/",
+        "teaser": "/assets/img/2019-07-01/zip2.png"
+      },{
+        "title": "Post: Quote",
+        "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["Blog"],
+        "tags": ["Post Formats","quote"],
+        "url": "/blog/post-quote/",
+        "teaser": "/assets/img/2019-07-01/zip2.png"
+      },{
+        "title": "Post: Link",
+        "excerpt":"This theme supports link posts, made famous by John Gruber. To use, just add link: http://url-you-want-linked to the post’s YAML front matter and you’re done.      And this is how a quote looks.    Some link can also be shown.  ","categories": ["Blog"],
+        "tags": ["link","Post Formats"],
+        "url": "/blog/post-link/",
+        "teaser": "/assets/img/2019-07-01/zip2.png"
+      },{
+        "title": "Exploring a computer network from its netflow traffic I",
+        "excerpt":"This blog takes a look at (part of) a data set released a few years ago by Los Alamos US National Labs [1]. There are five files in all, consisting of anonymised netflow, DNS records, authentication events, Windows process and red-team events on the lab’s internal network. In this first...","categories": ["data_science"],
+        "tags": [],
+        "url": "/data_science/lanl-netflow-1/",
+        "teaser": "/assets/img/2018-07-01/netflow.png"
+      },{
+        "title": "Exploring a computer network from its netflow traffic II",
+        "excerpt":"In the previous post I showed a visualisation of 10,000 computers as points in the plane, on the basis of their profiles of port and protocol usage. This second post has two motivations (and I hope they won’t trip over each other). The first is unfinished business from the first...","categories": ["data_science"],
+        "tags": [],
+        "url": "/data_science/lanl-netflow-2/",
+        "teaser": "/assets/img/2018-08-01/lanl_drl.jpg"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/welcome-to-jekyll/",
+        "teaser": "/assets/img/2019-07-01/zip2.png"
+      },{
+        "title": "A topological trick for data visualisation",
+        "excerpt":"How do hand-written digits arrange themselves in pixel space? The plot above (and the interactive version linked to it) illustrates the ‘mapper’ construction described in Gunnar Carlsson, Topology and Data, Bull Amer Math Soc 46 (2009) — here applied to the well known ‘Zip’ data set consisting of 7,291 hand-written...","categories": ["data_science"],
+        "tags": [],
+        "url": "/data_science/topological-trick-for-data-visualisation/",
+        "teaser": "/assets/img/2019-07-01/zip2.png"
+      },{
+        "title": "A demo of the $t$-test",
+        "excerpt":"This one was for my daughter, who at the time was learning about the $t$-test. The demo linked above shows how the two-sample (Welch) $t$-test works. The idea is that there are two random variables (or populations) $A$ and $B$, each normally distributed, and we want to test whether these...","categories": ["data_science"],
+        "tags": [],
+        "url": "/data_science/demo-of-the-ttest/",
+        "teaser": "/assets/img/2019-08-01/ttest.png"
+      },{
+        "title": "A word2vec map of Wikipedia words",
+        "excerpt":"This is an update of a little exercise, a few years ago, in making my own word2vec map of Wikipedia words. At that time, I started with 72,000 Wikipedia words and their word2vec representations in 200-dimensional space. In order to visualise that cloud of points, I made a graph by...","categories": ["data_science"],
+        "tags": [],
+        "url": "/data_science/wikiwords/",
+        "teaser": "/assets/img/2019-09-01/wikimap.jpg"
+      },{
+        "title": "Mean-squared error versus cross-entropy",
+        "excerpt":"This post is about multinomial probability distributions $p = (p_1, \\ldots ,p_N)$ where $\\sum_i p_i = 1$. We often need to compare two such distributions using a ‘distance’ function $d(p,q)$. For example (and the motivation for this blog post was a recent conversation about this case), the error that gets...","categories": ["data_science"],
+        "tags": [],
+        "url": "/data_science/crossent/",
+        "teaser": "/assets/img/2019-10-01/crossent.png"
+      },{
+        "title": "Watching a neural network learn a Markov chain",
+        "excerpt":"Modern language models derive their power from big data and big compute – but also, ultimately, from the Unreasonable Effectiveness of Recurrent Neural Networks described by Andrej Karpathy (and many others) a decade or so ago. This post is more low-brow than Karpathy’s — I wanted to explore a little...","categories": ["data_science"],
+        "tags": [],
+        "url": "/data_science/rnn-learns-markov/",
+        "teaser": "/assets/img/2019-11-01/ex3_3_5_2_3D_bystate.png"
+      },{
+        "title": "COP26: the climate crisis today",
+        "excerpt":"This is the first of three blogs in which I’d like to share a few thoughts on the climate crisis and on the COP26 conference which launches in Glasgow this week. In the second blog I’ll say a bit about the figure of 1.5 degrees warming and what it means;...","categories": ["environment"],
+        "tags": [],
+        "url": "/environment/cop26-today/",
+        "teaser": "/assets/img/2021-11-01/image1.png"
+      },{
+        "title": "COP26: why 1.5 degrees?",
+        "excerpt":"In the 2015 Paris Agreement, countries of the world signed up to “keep the rise in average global temperature to well below 2°C above pre-industrial levels, and preferably limit the increase to 1.5°C”. This month’s COP26 summit is the last best chance for nations to agree plans of action to...","categories": ["environment"],
+        "tags": [],
+        "url": "/environment/cop26-why1pt5/",
+        "teaser": "/assets/img/2021-11-02/image1.jpeg"
+      },{
+        "title": "COP26: seeing the wood for the trees",
+        "excerpt":"The climate crisis poses an almost overwhelming challenge to humanity, and it is very easy to be pessimistic. But there are also some reasons to be hopeful. Many will have been inspired by Prince William’s Earthshot initiative, by the vision behind it and the passion and creativity of the finalists...","categories": ["environment"],
+        "tags": [],
+        "url": "/environment/cop26-nzs/",
+        "teaser": "/assets/img/2021-11-03/woodfortrees1.jpg"
+      },{
+        "title": "Navigating the PLOS ONE topic tree",
+        "excerpt":"PLOS ONE is a respected multidisciplinary journal publishing research from over two hundred subject areas across science, engineering, medicine, and the related social sciences and humanities. But exactly how many subject areas? At the top level (as shown in the screenshot above), these subject areas fall under eleven headings from...","categories": ["data_science"],
+        "tags": [],
+        "url": "/data_science/plosone-topic-tree/",
+        "teaser": "/assets/img/2022-02-10/browse_topics.png"
+      },{
+        "title": "Language barriers in global conservation",
+        "excerpt":"Wildlife tends not to respect national boundaries. Birds, in particular, as they migrate across and between continents, ignore not only borders but even the cultures and languages of the scientists who may be trying to study and protect them. And, surprise surprise, not all of the world’s science is written...","categories": ["environment"],
+        "tags": [],
+        "url": "/environment/language_barriers/",
+        "teaser": "/assets/img/2023-02-22/birdclouds.jpg"
+      }]
